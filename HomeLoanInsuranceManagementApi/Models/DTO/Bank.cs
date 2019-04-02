f9b02a68f6f62431e7ccee21cae2f1769c463c9d
pavanmca17 <33794943+pavanmca17@users.noreply.github.com>
2019-04-02 18:46:48 +0530

    Update Entity

--- HomeLoanInsuranceManagementApi/Models/DTO/Bank.cs
+++ HomeLoanInsuranceManagementApi/Models/DTO/Bank.cs
@@ -3,7 +3,8 @@
 namespace HomeLoanInsuranceManagementApi.Models
 {
     public class Bank : BaseEntity
-    {
-
+    {   
+       public List<string> borrowerIds { get; set; }
+       public List<string> propertiesIds { get; set; }
     }
 }
